04017b9ebb8ba305ba5785d998b2e2d72ac45616
Radek <olidab@microsoft.com>
2016-04-07 07:40:00 -0700

    using os hostname instead

--- gallant-gazelle/server.js
+++ gallant-gazelle/server.js
@@ -1,11 +1,12 @@
 'use strict';
 
 const express = require('express');
+const os = require('os');
 let app = express();
 let port = process.env.PORT || 3000;
 
 app.get('/info', function (req, res) {
-  res.send({host: req.hostname, time: new Date()});
+  res.send({host: os.hostname(), time: new Date()});
 });
 
 app.listen(port, function () {
